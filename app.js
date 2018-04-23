ec0b554da48c46fbadb5df2a6ecd9b943e52e50f
Immad Hamid <immad.hamid@gmail.com>
2018-04-24 03:12:13 +0500

    - Adeed an event to listen to the click event and did some validations

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // game values
 let min = 1,
     max = 10,
-    winningNum = 2,
+    winningNum = Math.floor(Math.random() * (max - min + 1) + min),
     guessLeft = 3;
+
+console.log(`Winning Number: ${winningNum}`);
 
 // ui variables
 const gameEl = document.querySelector('#game'),
@@ -17,4 +19,32 @@ minEl.textContent = min;
 maxEl.textContent = max;
 
 // listen guess button
-// guessBtnEl.addEventListener();
+guessBtnEl.addEventListener('click', (e) => {
+    console.log('Submitted');
+    const guess = parseInt(guessInputEl.value);
+
+    if (isNaN(guess) || guess < min || guess > max) {
+        setMessage(`Please enter a message between ${min} & ${max}`, 'red');
+    } else {
+        if (guessLeft <= 1) {
+            guessBtnEl.disabled = true;
+            setMessage(`Game Over`);
+        } else {
+            if (guess === winningNum) {
+                guessBtnEl.disabled = true;
+                setMessage(`You are right ${winningNum} is the right answer`, 'green');
+            } else {
+                guessLeft = guessLeft - 1;
+                setMessage(`You have ${guessLeft} turns left`, 'initial');
+            }
+        }
+    }
+
+    e.preventDefault();
+});
+
+function setMessage(msg, color) {
+    message.style.color = color;
+    message.textContent = msg;
+    guessInputEl.style.borderColor = color;
+}
